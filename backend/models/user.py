aaa828f94ce0b4556eb79a6f35ef02738c90f58f
Aadit <aadit2127@gmail.com>
2026-06-28 23:26:37 +0100

    add JWT authentication p2

--- backend/models/user.py
+++ backend/models/user.py
@@ -9,7 +9,7 @@ from models.base import Base
 
 class User(Base):
     __tablename__ = "users"
-    
+
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,)
     email: Mapped[str] = mapped_column(String,unique=True,nullable=False,)
     password_hash: Mapped[str] = mapped_column(String, nullable=False)
